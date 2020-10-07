91809cdfd7170ddeca91dcd7ab3a355ef759fe91
Julio Segarra <42501108+juliosegarra@users.noreply.github.com>
2020-10-07 12:17:42 -0400

    Initial basic UI and JS implemented

---
+++ BrowserUI/Scripts/custom_scripts.js
@@ -0,0 +1,45 @@
+// ids = reverseButton \ inputString \ outputString
+
+// assign our button to a const
+const button = document.getElementById("reverseButton");
+
+// throw an addEventListener on button
+button.addEventListener("click", function () {
+    /***** VARIABLE DECLARATIONS *****/
+    const inputString = document.getElementById("inputString");
+    const outputString = document.getElementById("outputString");
+    let lowerInput = inputString.value;
+    lowerInput = lowerInput.toLowerCase();
+    // REGEX TO CLEAN INPUT DATA
+    lowerInput = lowerInput.replace(/[^a-z]/gi, "");
+
+    /***** FUNCTION DECLARATIONS *****/
+    const reverseString = (str) => {
+        let reversedWord = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+            reversedWord += str.charAt(i);
+        }
+        return reversedWord;
+    };
+
+    const isPalindrome = (str) => {
+        let firstIndex = 0;
+        let lastIndex = str.length - 1;
+        while (firstIndex < lastIndex) {
+            if (str.charAt(firstIndex) != str.charAt(lastIndex)) {
+                return "This word is NOT a Palindrome.";
+            }
+            firstIndex++;
+            lastIndex--;
+        }
+        return "This is word is a Palindrome!";
+    };
+
+    const inputToInner = (inString, outString) => {
+        outString.innerHTML = `${inString}\n\n${isPalindrome(inString)}`;
+    };
+
+    /***** CODE EXECUTION *****/
+    lowerInput = reverseString(lowerInput);
+    inputToInner(lowerInput, outputString);
+});
